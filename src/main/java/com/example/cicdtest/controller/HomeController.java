f40cac34a97961a3817b34f647f41c4ce6f0b74b
tjsdn9803 <leeinjoo2@inu.ac.kr>
2023-07-15 18:07:05 +0900

    init commit

---
+++ src/main/java/com/example/cicdtest/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.cicdtest.controller;
+
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/api/hello")
+    public String helloWorld() {
+        return "Hello world";
+    }
+}
