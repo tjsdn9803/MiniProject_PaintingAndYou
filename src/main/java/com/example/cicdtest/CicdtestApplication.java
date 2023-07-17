080459a10ac9387949606570756b7ad69039c143
tjsdn9803 <leeinjoo2@inu.ac.kr>
2023-07-17 14:23:46 +0900

    프론트 Origin허용

--- src/main/java/com/example/cicdtest/CicdtestApplication.java
+++ src/main/java/com/example/cicdtest/CicdtestApplication.java
@@ -2,7 +2,10 @@ package com.example.cicdtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -10,6 +13,16 @@ public class CicdtestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CicdtestApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://paintings-and-you.s3-website.ap-northeast-2.amazonaws.com");
+            }
+        };
     }
 
 }
