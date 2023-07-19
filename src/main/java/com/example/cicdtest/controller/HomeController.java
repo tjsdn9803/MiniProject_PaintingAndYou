8c1b2ccb9492bb01214e3b4539e7f061cbaf28be
tjsdn9803 <leeinjoo2@inu.ac.kr>
2023-07-19 22:36:40 +0900

    jwt 인가 처리 확인 테스트

--- src/main/java/com/example/cicdtest/controller/HomeController.java
+++ src/main/java/com/example/cicdtest/controller/HomeController.java
@@ -1,6 +1,9 @@
 package com.example.cicdtest.controller;
 
+import com.example.cicdtest.entity.User;
+import com.example.cicdtest.security.UserDetailsImpl;
 import lombok.Getter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +14,11 @@ public class HomeController {
     public String helloWorld() {
         return "Hello world CI/CD test!!";
     }
+
+    @GetMapping("/api/auth")
+    public String userAuth(@AuthenticationPrincipal UserDetailsImpl userDetails){
+        User user = userDetails.getUser();
+        return user.getUsername();
+    }
+
 }
