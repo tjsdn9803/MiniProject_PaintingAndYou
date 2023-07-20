0dec7794bc87164565e894302462cf9d6660a3a8
tjsdn9803 <leeinjoo2@inu.ac.kr>
2023-07-20 10:10:20 +0900

    allowCredential : false

--- src/main/java/com/example/cicdtest/CicdtestApplication.java
+++ src/main/java/com/example/cicdtest/CicdtestApplication.java
@@ -20,7 +20,11 @@ public class CicdtestApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://paintings-and-you.s3-website.ap-northeast-2.amazonaws.com", "http://localhost:3000").allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
+                registry.
+                        addMapping("/**")
+                        .allowedOrigins("http://paintings-and-you.s3-website.ap-northeast-2.amazonaws.com", "http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowCredentials(false);
             }
         };
     }
